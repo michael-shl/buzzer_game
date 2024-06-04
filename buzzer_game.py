6d73b5190785fe492d4d1624c9b07cbb7eccbdbe
michael-shl <lish1998@gmail.com>
2024-06-04 17:31:59 -0400

    fix the led global variable bug and add flash led

--- buzzer_game.py
+++ buzzer_game.py
@@ -6,14 +6,14 @@ import os
 # Variables
 count = 0
 delay1 = 0.1
-delay2 = 5
+delay2 = 2
 pin_index = 0  # Start with the first LED in the list
 green_pin = 27
 yellow_pin = 17
 sounds_dir = '/home/eastonli/buzzer_game/sounds'
 
 # Setup LEDs and buttons
-led_pins = [4, 5, 6]
+led_pins = [4, 5, 6, 13, 19, 26]
 leds = [LED(pin) for pin in led_pins]
 green_button = Button(green_pin, pull_up=True)
 yellow_button = Button(yellow_pin, pull_up=True)
@@ -47,7 +47,7 @@ reset_led()
 play_sound("Gong")
 
 def check_yellow():
-    global count
+    global count, pin_index
     if yellow_button.is_pressed:
         play_sound("zoop")
         turn_on_led()
@@ -66,8 +66,11 @@ def check_yellow():
             play_sound("Gong")
 
 def check_green():
-    global count
+    global count, pin_index
     if green_button.is_pressed:
+        leds[3].on()
+        leds[4].on()
+        leds[5].on()
         play_sound("dance_around")
         speak(f"You win! Game will restart in {delay2} seconds")
         time.sleep(delay2)
